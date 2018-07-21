83b0ee5324cc8f0f64b1a21e07754d9830d8e60d
Hoang Hung <hth321@gmail.com>
2018-07-21 14:04:50 +0700

    [webpack] fixed Buffer warning deprecation

--- src/webpack.js
+++ src/webpack.js
@@ -13,7 +13,7 @@ const base64Encode = file => {
   // read binary data
   const bitmap = fs.readFileSync(file);
   // convert binary data to base64 encoded string
-  return new Buffer(bitmap).toString('base64');
+  return new Buffer.from(bitmap).toString('base64');
 };
 
 /**
